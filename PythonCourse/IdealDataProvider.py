a8e4130150953aded81268e9b50fdf482082c7db
Dejan Seguljev <dejan.seguljev@thelogosvision.com>
2023-10-02 07:18:38 -0700

    Loading ideal data

--- PythonCourse/IdealDataProvider.py
+++ PythonCourse/IdealDataProvider.py
@@ -6,10 +6,10 @@ class IdealDataProvider(DataProvider):
         super().__init__(connection_string, csv_path, table_name)        
 
       def prepareData(self, data_list):
-        return list(map(lambda x: {"x":x[0], "y1":x[1], "y2":x[2], "y3":x[3], "y4":x[4]}, data_list.values.tolist()))        
+        return list(map(lambda x: {"x":x[0], "y1":x[1], "y2":x[2], "y3":x[3], "y4":x[4], "y5":x[5], "y6":x[6], "y7":x[7], "y8":x[8], "y9":x[9], "y10":x[10], "y11":x[11], "y12":x[12], "y13":x[13], "y14":x[14], "y15":x[15], "y16":x[16], "y17":x[17], "y18":x[18], "y19":x[19], "y20":x[20], "y21":x[21], "y22":x[22], "y23":x[23], "y24":x[24], "y25":x[25], "y26":x[26], "y27":x[27], "y28":x[28], "y29":x[29], "y30":x[30], "y31":x[31], "y32":x[32], "y33":x[33], "y34":x[34], "y35":x[35], "y36":x[36], "y37":x[37], "y38":x[38], "y39":x[39], "y40":x[40], "y41":x[41], "y42":x[42], "y43":x[43], "y44":x[44], "y45":x[45], "y46":x[46], "y47":x[47], "y48":x[48], "y49":x[49], "y50":x[50]}, data_list.values.tolist()))        
               
       def createMetadata(self, table_name, meta_data):
-        return MetadataProvider.createTrainingMetadata(meta_data)        
+        return MetadataProvider.createIdealMetadata(meta_data)        
 
 
 
--- PythonCourse/MetadataProvider.py
+++ PythonCourse/MetadataProvider.py
@@ -2,6 +2,15 @@ import sqlalchemy as db
 
 class MetadataProvider(object):
     @staticmethod    
+
+    def createTestMetadata(meta_data):
+      return db.Table("test_functions", meta_data, 
+        db.Column("id", db.Integer, primary_key=True, autoincrement=True, nullable=False),
+        db.Column("x", db.Float, nullable=True),
+        db.Column("y", db.Float, nullable=True),
+        db.Column("delta_y", db.Float, nullable=True),
+        db.Column("ideal_count", db.Integer, nullable=True))          
+           
     def createTrainingMetadata(meta_data):
       return db.Table("training_functions", meta_data, 
         db.Column("id", db.Integer, primary_key=True, autoincrement=True, nullable=False),
@@ -12,13 +21,59 @@ class MetadataProvider(object):
         db.Column("y4", db.Float, nullable=True))          
 
     def createIdealMetadata(meta_data):
-      return db.Table("training_functions", meta_data, 
+      return db.Table("ideal_functions", meta_data, 
         db.Column("id", db.Integer, primary_key=True, autoincrement=True, nullable=False),
         db.Column("x", db.Float, nullable=True),
         db.Column("y1", db.Float, nullable=True),
         db.Column("y2", db.Float, nullable=True),
         db.Column("y3", db.Float, nullable=True),
-        db.Column("y4", db.Float, nullable=True))       
+        db.Column("y4", db.Float, nullable=True),
+        db.Column("y5", db.Float, nullable=True),     
+        db.Column("y6", db.Float, nullable=True),
+        db.Column("y7", db.Float, nullable=True),
+        db.Column("y8", db.Float, nullable=True),
+        db.Column("y9", db.Float, nullable=True),
+        db.Column("y10", db.Float, nullable=True),
+        db.Column("y11", db.Float, nullable=True),
+        db.Column("y12", db.Float, nullable=True),
+        db.Column("y13", db.Float, nullable=True),
+        db.Column("y14", db.Float, nullable=True),
+        db.Column("y15", db.Float, nullable=True),    
+        db.Column("y16", db.Float, nullable=True),
+        db.Column("y17", db.Float, nullable=True),
+        db.Column("y18", db.Float, nullable=True),
+        db.Column("y19", db.Float, nullable=True),
+        db.Column("y20", db.Float, nullable=True),
+        db.Column("y21", db.Float, nullable=True),
+        db.Column("y22", db.Float, nullable=True),
+        db.Column("y23", db.Float, nullable=True),
+        db.Column("y24", db.Float, nullable=True),
+        db.Column("y25", db.Float, nullable=True),
+        db.Column("y26", db.Float, nullable=True),
+        db.Column("y27", db.Float, nullable=True),
+        db.Column("y28", db.Float, nullable=True),
+        db.Column("y29", db.Float, nullable=True),
+        db.Column("y30", db.Float, nullable=True),
+        db.Column("y31", db.Float, nullable=True),
+        db.Column("y32", db.Float, nullable=True),
+        db.Column("y33", db.Float, nullable=True),
+        db.Column("y34", db.Float, nullable=True),
+        db.Column("y35", db.Float, nullable=True),
+        db.Column("y36", db.Float, nullable=True),
+        db.Column("y37", db.Float, nullable=True),
+        db.Column("y38", db.Float, nullable=True),
+        db.Column("y39", db.Float, nullable=True),
+        db.Column("y40", db.Float, nullable=True),
+        db.Column("y41", db.Float, nullable=True),
+        db.Column("y42", db.Float, nullable=True),
+        db.Column("y43", db.Float, nullable=True),
+        db.Column("y44", db.Float, nullable=True),
+        db.Column("y45", db.Float, nullable=True),   
+        db.Column("y46", db.Float, nullable=True),
+        db.Column("y47", db.Float, nullable=True),
+        db.Column("y48", db.Float, nullable=True),
+        db.Column("y49", db.Float, nullable=True),
+        db.Column("y50", db.Float, nullable=True))         
 
 
 
